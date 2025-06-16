602ba89606d9ce990f0ebfcd6fa4bab717a700ba
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-06-16 15:33:08 +0200

    Implement a class that represents the Registration page

---
+++ page-objects/pages/registration/registration.page.ts
@@ -0,0 +1,20 @@
+import type { Locator, Page } from '@playwright/test';
+import NavBar from '../../global-components/navbar';
+import RegistrationForm from './components/registration-form';
+import { BasePage } from '../base.page';
+
+export default class RegistrationPage extends BasePage {
+    readonly path: string = '/register'
+    readonly navbar: NavBar;
+    readonly registrationForm: RegistrationForm;
+
+    constructor(page: Page) {
+        super(page);
+        this.navbar = new NavBar(page);
+        this.registrationForm = new RegistrationForm(page);
+    }
+
+    async open(): Promise<void> {
+        await super.open(`${process.env.BASE_URL}${this.path}`);
+    }
+}
