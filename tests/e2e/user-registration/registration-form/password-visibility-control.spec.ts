3004e439b2fd64a459168bbd82317ae378c23d7f
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-06-16 15:40:16 +0200

    Create e2e tests that cover password visibility control in the Registration form

---
+++ tests/e2e/user-registration/registration-form/password-visibility-control.spec.ts
@@ -0,0 +1,114 @@
+import { test, expect } from '../../../../page-objects/test.fixture';
+import { User } from 'types';
+
+test.describe('@desktop Password Visibility Toggle', () => {
+    let user: User;
+
+    test.beforeEach(async ({ registrationPage, page, dataFactory }) => {
+        // Open Registration page
+        await registrationPage.open();
+
+        await expect(page).toHaveURL(new RegExp(registrationPage.path), {
+            timeout: 10000
+        });
+
+        // Prepare test data
+        user = dataFactory.createValidUserData();
+    });
+
+    test(
+        'Password value is hidden by default',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.enterPassword(user.password!);
+
+            await expect(
+                registrationPage.registrationForm.passwordField
+            ).toHaveAttribute('type', 'password');
+        }
+    );
+
+    test(
+        'Confirm Password value is hidden by default',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.confirmPassword(user.password!);
+
+            await expect(
+                registrationPage.registrationForm.confirmPasswordField
+            ).toHaveAttribute('type', 'password');
+        }
+    );
+
+    test(
+        'User can reveal password by clicking the visibility toggle',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.enterPassword(user.password!);
+
+            registrationPage.registrationForm.showPassword();
+
+            await expect(
+                registrationPage.registrationForm.passwordField
+            ).toHaveAttribute('type', 'text');
+        }
+    );
+
+    test(
+        'User can reveal confirmation password by clicking the visibility toggle',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.confirmPassword(user.password!);
+
+            registrationPage.registrationForm.showConfirmationPassword();
+
+            await expect(
+                registrationPage.registrationForm.confirmPasswordField
+            ).toHaveAttribute('type', 'text');
+        }
+    );
+
+    test(
+        'User can hide password by clicking the visibility toggle',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.enterPassword(user.password!);
+
+            registrationPage.registrationForm.showPassword();
+
+            registrationPage.registrationForm.hidePassword();
+
+            await expect(
+                registrationPage.registrationForm.passwordField
+            ).toHaveAttribute('type', 'password');
+        }
+    );
+
+    test(
+        'User can hide confirmation password by clicking the visibility toggle',
+        { tag: ['@regression', '@registration'] },
+        async ({ registrationPage }) => {
+            /** STEPS **/
+
+            registrationPage.registrationForm.confirmPassword(user.password!);
+
+            registrationPage.registrationForm.showConfirmationPassword();
+
+            registrationPage.registrationForm.hideConfirmationPassword();
+
+            await expect(
+                registrationPage.registrationForm.confirmPasswordField
+            ).toHaveAttribute('type', 'password');
+        }
+    );
+});
