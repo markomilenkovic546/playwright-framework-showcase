426db9fb2676bf9d29ee2fe79a742c967d1b4416
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-06-16 15:31:43 +0200

    Implement a class that represents the Product Listing Page

---
+++ page-objects/pages/plp/product-listing.page.ts
@@ -0,0 +1,24 @@
+import type { Locator, Page } from '@playwright/test';
+import NavBar from '../../global-components/navbar';
+import ProductCard from './components/product-card';
+import FilterPanel from './components/filter-panel';
+import { BasePage } from '../base.page';
+
+export default class ProductListingPage extends BasePage {
+    readonly path: string = '/register'
+    readonly navbar: NavBar;
+    readonly productCard: ProductCard;
+    readonly filterPanel: FilterPanel;
+  
+
+    constructor(page: Page) {
+        super(page);
+        this.navbar = new NavBar(page);
+        this.productCard = new ProductCard(page)
+        this.filterPanel = new FilterPanel(page)
+    }
+
+    async open(): Promise<void> {
+        await super.open(`${process.env.BASE_URL}${this.path}`);
+    }
+}
