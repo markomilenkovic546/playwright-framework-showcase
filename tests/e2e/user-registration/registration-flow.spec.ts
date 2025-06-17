5c7d9eeb103d0039e34bc488119d65d0074aa35c
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-06-17 18:49:24 +0200

    Create e2e tests that cover e2e registration flow

---
+++ tests/e2e/user-registration/registration-flow.spec.ts
@@ -0,0 +1,107 @@
+import { test, expect } from '../../../page-objects/test.fixture';
+import { User } from 'types';
+
+test.describe('@desktop Registration flow', () => {
+    test.beforeEach(async ({ registrationPage, page }) => {
+        // Open Registration page
+        await registrationPage.open();
+
+        await expect(page).toHaveURL(new RegExp(registrationPage.path), {
+            timeout: 10000
+        });
+    });
+
+    test(
+        'User can register successfully',
+        { tag: ['@smoke', '@registration'] },
+        async ({ registrationPage, loginPage, page, dataFactory }) => {
+            // Prepare test data
+            const user: User = dataFactory.createValidUserData();
+
+            /** STEPS **/
+            await registrationPage.registrationForm.fillForm(user);
+
+            await registrationPage.registrationForm.submitForm();
+
+            await expect(page).toHaveURL(new RegExp(loginPage.path), {
+                timeout: 10000
+            });
+
+            await loginPage.login(user.username!, user.password!);
+
+            await expect(loginPage.navbar.accountDropdownButton).toContainText(
+                user.username!
+            );
+        }
+    );
+});
+
+test.describe('@mobile Registration flow', () => {
+    test.beforeEach(async ({ registrationPage, page }) => {
+        // Open Registration page
+        await registrationPage.open();
+
+        await expect(page).toHaveURL(new RegExp(registrationPage.path), {
+            timeout: 10000
+        });
+    });
+
+    test(
+        'User can register successfully',
+        { tag: ['@smoke', '@registration'] },
+        async ({ registrationPage, loginPage, page, dataFactory }) => {
+            // Prepare test data
+            const user: User = dataFactory.createValidUserData();
+
+            /** STEPS **/
+            await registrationPage.registrationForm.fillForm(user);
+
+            await registrationPage.registrationForm.submitForm();
+
+            await expect(page).toHaveURL(new RegExp(loginPage.path), {
+                timeout: 10000
+            });
+
+            await loginPage.login(user.username!, user.password!);
+
+            await expect(loginPage.navbar.accountDropdownButton).toContainText(
+                user.username!
+            );
+        }
+    );
+});
+
+test.describe('@tablet Registration flow', () => {
+    test.beforeEach(async ({ registrationPage, page }) => {
+        // Open Registration page
+        await registrationPage.open();
+
+        await expect(page).toHaveURL(new RegExp(registrationPage.path), {
+            timeout: 10000
+        });
+    });
+
+    test(
+        'User can register successfully',
+        { tag: ['@smoke', '@registration'] },
+        async ({ registrationPage, loginPage, page, dataFactory }) => {
+            // Prepare test data
+            const user: User = dataFactory.createValidUserData();
+
+            /** STEPS **/
+            await registrationPage.registrationForm.fillForm(user);
+
+            await registrationPage.registrationForm.submitForm();
+
+            await expect(page).toHaveURL(new RegExp(loginPage.path), {
+                timeout: 10000
+            });
+
+            await loginPage.login(user.username!, user.password!);
+
+            await expect(loginPage.navbar.accountDropdownButton).toContainText(
+                user.username!
+            );
+        }
+    );
+});
