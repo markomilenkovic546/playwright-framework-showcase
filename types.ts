70d0dcd8b130a7cba43173cc57a20b664546f5d0
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-06-18 18:48:10 +0200

    Add types/interfaces

--- types.ts
+++ types.ts
@@ -5,3 +5,22 @@ export interface User {
     password?: string;
     gender: string;
 }
+
+export interface Product {
+    bookId: number;
+    title: string;
+    author: string;
+    category: string;
+    price: number;
+    coverFileName: string;
+}
+
+export type FilterOptions = string[];
+
+export interface ShippingAddress {
+    name?: string;
+    addressLine1?: string;
+    addressLine2?: string;
+    pincode?: string;
+    state?: string;
+}
